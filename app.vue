6376752949248a740ee85664d9db913dc5561689
vitalii.grebenik <vitalii.grebenik@latoken.com>
2023-03-01 17:22:35 +0300

    fixed code

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
+    <Head>
+      <Title>Auth form Shtab</Title>
+      <Meta name="description" content="Auth form Shtab"/>
+    </Head>
     <NuxtPage/>
-  </div>
 </template>
