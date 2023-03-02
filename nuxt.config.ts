b168b53d97eda1b50f1f3f36b54fd2febf2f7777
vitalii.grebenik <vitalii.grebenik@latoken.com>
2023-03-02 23:21:16 +0300

    fixed mobile adaptive

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,7 @@ export default defineNuxtConfig({
   app: {
     head: {
       charset: "utf-8",
-      viewport:
-        "width=device‑width, height=device‑height, initial‑scale=1.0, maximum‑scale=1.0, minimum‑scale=1.0, user‑scalable=no",
+      // viewport: "width=device‑width, initial‑scale=1",
       title: "Form Auth Shtab",
       meta: [{ name: "description", content: "Form Auth Shtab" }],
       link: [
